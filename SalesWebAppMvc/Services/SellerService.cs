a4aad6a53785a75fe5544d55d41f03d53eee37ea
Leonardo Bortolini Trombini <leeonardobt@gmail.com>
2022-03-28 20:43:57 -0300

    Criado SellersController

---
+++ SalesWebAppMvc/Services/SellerService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebAppMvc.Models;
+using SalesWebAppMvc.Data;
+
+namespace SalesWebAppMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebAppMvcContext _context;
+
+        public SellerService(SalesWebAppMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+    }
+}
--- SalesWebAppMvc/Startup.cs
+++ SalesWebAppMvc/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebAppMvc.Data;
+using SalesWebAppMvc.Services;
 
 namespace SalesWebAppMvc
 {
@@ -41,6 +42,7 @@ namespace SalesWebAppMvc
                         builder.MigrationsAssembly("SalesWebAppMvc")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
