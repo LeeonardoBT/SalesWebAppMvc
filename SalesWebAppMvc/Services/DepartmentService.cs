097d95695a9ea9b823e465606926b106ae13bb30
Leonardo Bortolini Trombini <leeonardobt@gmail.com>
2022-03-28 21:15:04 -0300

    Criata Foreingkey para Seller e Department

---
+++ SalesWebAppMvc/Services/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using SalesWebAppMvc.Data;
+using SalesWebAppMvc.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebAppMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly SalesWebAppMvcContext _context;
+
+        public DepartmentService(SalesWebAppMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
