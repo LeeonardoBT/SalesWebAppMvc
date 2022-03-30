153b8750a52d5887b760f5c6f77c31b6701adecc
Leonardo Bortolini Trombini <leeonardobt@gmail.com>
2022-03-30 20:35:46 -0300

    Validation

--- SalesWebAppMvc/Controllers/SellersController.cs
+++ SalesWebAppMvc/Controllers/SellersController.cs
@@ -37,6 +37,13 @@ namespace SalesWebAppMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(seller);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -106,7 +113,14 @@ namespace SalesWebAppMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
-            if(id != seller.Id)
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(seller);
+            }
+
+            if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
